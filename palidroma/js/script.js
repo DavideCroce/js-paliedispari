43052627209960a396c77045b2d25d51fb905c55
IskahnX <davidecroce10@gmail.com>
2022-01-14 15:13:35 +0100

    start palindrome function

---
+++ palidroma/js/script.js
@@ -0,0 +1,23 @@
+console.log ('JS OK');
+
+/* Chiedere all'utente di inserire una parola,
+
+Creare una funzione per capire se quella parola sia palidroma o no. */
+
+
+const Word = prompt ('Inserisci una parola');
+console.log (Word);
+
+function reverseWord (Word){
+let reverseWord = '';
+    for (let i = Word.length - 1; i >= 0; i--){
+        reverseWord += Word[i];
+    }
+    return reverseWord;
+}
+
+if (reverseWord = Word){
+    return true;
+} else {
+    return false;
+}
